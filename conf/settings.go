8c03460ce3453063bf09989d285cd6d986453762
serg <aspirin_1988@mail.ru>
2017-03-15 14:01:26 +0600

    Init

--- conf/settings.go
+++ conf/settings.go
@@ -2,7 +2,7 @@ package conf
 
 
 const (
-	BOT_TOKEN string = "225366019:AAGDMEeCVLARyx8A5g5BAUHqjMAPjR7AFOQ"
+	BOT_TOKEN string = "371139318:AAHYqiYcvAp4bzJrWJbh5K2BiRcH3KKpYiU"
 
 	APIEndpoint string = "https://api.telegram.org/bot%s/%s"
 
